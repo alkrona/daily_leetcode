18f298b650f5f3557ea1c03fc726873519059e3f
alkrona <alkrona117@gmail.com>
2024-04-02 19:31:57 +1100

    updated code

--- april_2/april_2.py
+++ april_2/april_2.py
@@ -3,13 +3,19 @@ def isIsomorphic(s:str, t :str)->bool:
     for index,char in enumerate(s):
         if char not in dict:
             #print(f"{char}not in here ")
-            dict[char]= t[index]
-        elif char in dict:
+            if t[index] not in dict.values():
+                 
+                dict[char]= t[index]
+            elif t[index] in dict.values():
+                 return False
+        elif char in dict :
             #print(f"{char }in here")
-            if dict[char]==t[index]:
-                pass
-            else:
-                return False
+            
+                if dict[char]==t[index]:
+                    pass
+                else:
+                    return False
+            
     return True
 if __name__ =="__main__":
-    print(isIsomorphic("egg","add")) 
+    print(isIsomorphic("badc","baba")) 
